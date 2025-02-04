9246228c641c9ac105e764cfddf85720ec41b396
Daniel Marques <42500719+danielrommel@users.noreply.github.com>
2025-02-04 17:36:53 -0300

    Criação das variaveis e funções resposaveis por adicionar nomes dentro de uma  lista e exibir essa lista com os nomes

--- challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// VARIAVEIS
+let nome;
+let listaAmigos;
+let elementLista;
+let li;
+
+// valorSorteado = parseInt(Math.random() * listaAmigos + 1)
+listaAmigos = [];
+
+function adicionar(){
+    nome = document.querySelector("input").value;
+    if(nome == ""){
+        alert("Erro, valor vazio. Tente inserir um valor!");
+    } else if (listaAmigos.includes(nome)){
+        alert("Erro, valor já existente. Tente inserir um diferente!");        
+    } else{
+        listaAmigos.push(nome);
+        nome = document.querySelector("input");
+        nome.value = "";
+        exibelista();
+    }
+}
+    function exibelista(){
+        elementLista = document.getElementById("listaAmigos");
+        elementLista.innerHTML = "";
+
+        listaAmigos.forEach(nomeAmigo => {
+            li = document.createElement("li");
+            li.textContent = nomeAmigo;
+            elementLista.appendChild(li);
+        })
+    }
+
+
+// FUNCOES
+// adicionarnomes;
+// validarentrada;
+// botaoadicionar;
+// exibirnomes;
+// botaosortear;
+// mostrarresultado;
+// CONDICOES
